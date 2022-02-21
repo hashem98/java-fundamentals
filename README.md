4748be801f7aefabed4e5fedf6dd852bca1b3b70
hashem98 <hashemsmadi98@gmail.com>
2022-02-21 13:17:10 +0200

    lab1 is done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-fundamentals
+ 
+ * Lab 01-Java primitives and control flow :  Main.java - Main class with pluralize , flipping coins, and command line clock methods.
---
+++ basics/Main.java
@@ -0,0 +1,61 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class Main {
+    public static String pluralize (String name,int count){
+        if (count==1){
+            return name;
+        }else{
+            return name+"s";
+        }
+    }
+    public static void main(String[] args) {
+
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+        int catCount = 20;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+        flipHeads(1);
+        clock();
+
+    }
+
+
+    public static void clock(){
+        while (true){
+            LocalDateTime now = LocalDateTime.now();
+            int hour = now.getHour();
+            int minute = now.getMinute();
+            int second = now.getSecond();
+            String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            while (second==LocalDateTime.now().getSecond()){
+                continue;
+            }
+            System.out.println(time);
+        }}
+    public static void flipHeads (int n){
+        Random random = new Random();
+        int counter=0 , flips=0;
+        while(counter<n)
+        {
+            double randomNum = random.nextDouble();
+            if(randomNum<0.5)
+            {
+                System.out.println("tails");
+                flips++;
+            }
+            else
+            {
+                System.out.println("heads");
+                counter++;
+                flips++;
+            }
+        }
+        System.out.println("It took "+flips+" flips to flip "+n+" head in a row.");
+    }
+}
